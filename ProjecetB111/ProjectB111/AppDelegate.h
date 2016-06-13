1249ab55f7a69d49b36b9b7a696a9dfadb857798
heyyouly <liuyang@liuyangdeMacBook-Pro.local>
2016-06-13 09:45:03 +0800

    我改了 APPdelegate 的一个东西

---
+++ ProjecetB111/ProjectB111/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ProjectB111
+//
+//  Created by 刘洋 on 16/6/13.
+//  Copyright © 2016年 LY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ProjecetB111/ProjectB111/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ProjectB111
+//
+//  Created by 刘洋 on 16/6/13.
+//  Copyright © 2016年 LY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
